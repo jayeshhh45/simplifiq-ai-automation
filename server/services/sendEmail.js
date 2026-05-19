4a19315a497c1108c0627357750880efb9268a31
Your Name <you@example.com>
2026-05-19 06:30:58 +0530

    Fixed Render Gmail SMTP issue

--- server/services/sendEmail.js
+++ server/services/sendEmail.js
@@ -3,14 +3,18 @@ import nodemailer from "nodemailer";
 const sendEmail = async (to, company, pdfPath) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
     const mailOptions = {
       from: process.env.EMAIL_USER,
